47d697e9be9ca6e567a5cce408e138533ce24297
Sergiy Piven <seregapiven@gmail.com>
2022-08-25 15:40:21 +0300

    Homework2

---
+++ script.js
@@ -0,0 +1,4 @@
+const firstNumber = prompt('Введите первое число');
+const secondNumber = prompt('Введите второе число');
+const result = Number(firstNumber) + Number(secondNumber);
+alert(`${firstNumber} + ${secondNumber} = ${result}`);
