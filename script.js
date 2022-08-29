8d519f5f7bdea13f9ccdc55252e50cb0a635fa9b
Sergiy Piven <seregapiven@gmail.com>
2022-08-29 20:24:40 +0300

    Homework2

--- script.js
+++ script.js
@@ -1,4 +1,49 @@
-const firstNumber = prompt('Введите первое число');
-const secondNumber = prompt('Введите второе число');
-const result = Number(firstNumber) + Number(secondNumber);
-alert(`${firstNumber} + ${secondNumber} = ${result}`);
+let firstNumber; // Первое число
+let secondNumber; // Второе число
+let operation;
+let result; // Результат операций
+// ввод данных
+do {
+  firstNumber = prompt('Введите первое число');
+} while (isNumberInvalid(firstNumber));
+firstNumber = Number(firstNumber); // Перевод в тип Number
+
+do {
+  secondNumber = prompt('Введите второе число');
+} while (isNumberInvalid(secondNumber));
+secondNumber = Number(secondNumber); // Перевод в тип Number
+ 
+do {
+  operation = prompt(`Выберете значение (+, -, * или /)`);
+  } while(isOperationInvalid(operation));
+
+// Функции для проверки валидности введеных данных
+  function isNumberInvalid(val){
+    return val === null || val === '' || isNaN(val);
+  }
+  function isOperationInvalid(val){
+  return !(operation == '+' || operation == '-' || operation == '*' || operation == '/');
+}
+// Результат вычислений
+  switch (operation) {
+    case '+':
+      result = firstNumber + secondNumber;
+      alert(`${firstNumber} + ${secondNumber}= ${result}`);
+      break
+    case '-':
+      result = firstNumber - secondNumber;
+      alert(`${firstNumber} - ${secondNumber}=${result}`);
+      break
+    case '*':
+      result = firstNumber * secondNumber;
+      alert(`${firstNumber} * ${secondNumber}=${result}`);
+      break
+    case '/':
+      result = firstNumber / secondNumber;
+      alert(`${firstNumber} / ${secondNumber}=${result}`);
+      break;
+  }
+
+
+
+
